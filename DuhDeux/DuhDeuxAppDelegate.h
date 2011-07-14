8b4cb62f321871c3d361b948a358971a06e59006
Anil Pais & Ian Zabel <pair+paisani+izabel@pivotallabs.com>
2011-07-14 19:02:29 -0400

    Initial Commit

---
+++ DuhDeux/DuhDeuxAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  DuhDeuxAppDelegate.h
+//  DuhDeux
+//
+//  Created by pivotal on 7/14/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DuhDeuxAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
